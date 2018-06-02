0c8edeb4f79fb27ebdd7eee88511bbdecf664182
Tom <t.oliver.ball@protonmail.ch>
2018-06-02 16:54:28 +0100

    Integrating LocationTracker

--- src/main/java/jenjinn/engine/boardstate/LocationTracker.java
+++ src/main/java/jenjinn/engine/boardstate/LocationTracker.java
@@ -6,6 +6,10 @@ package jenjinn.engine.boardstate;
 import static java.lang.Math.max;
 import static xawd.jflow.utilities.PredicateUtil.any;
 
+import java.util.Arrays;
+
+import jenjinn.engine.enums.BoardSquare;
+
 /**
  * @author t
  *
@@ -23,7 +27,27 @@ public final class LocationTracker {
 		if (any(i -> i < 0 || i > 63, locations)) {
 			throw new IllegalArgumentException();
 		}
-		System.arraycopy(locations, 0, locs, 0, max(9, locations.length));
+		pieceCount = max(9, locations.length);
+		System.arraycopy(locations, 0, locs, 0, pieceCount);
+	}
+
+	public long allLocs()
+	{
+		long allLocs = 0L;
+		for (int i = 0; i < pieceCount; i++) {
+			allLocs |= 1L << locs[i];
+		}
+		return allLocs;
+	}
+
+	public boolean contains(BoardSquare location)
+	{
+		for (int i = 0; i < pieceCount; i++) {
+			if (locs[i] == location.ordinal()) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public int indexLocs(int queryIndex)
@@ -36,27 +60,56 @@ public final class LocationTracker {
 		return pieceCount;
 	}
 
-	void addLoc(int loc)
+	public LocationTracker copy()
 	{
-		locs[pieceCount] = loc;
+		return new LocationTracker(Arrays.copyOf(locs, pieceCount));
+	}
+
+	void addLoc(BoardSquare location)
+	{
+		locs[pieceCount] = location.ordinal();
 		pieceCount++;
 	}
 
-	void removeLoc(int loc)
+	void removeLoc(BoardSquare location)
 	{
 		assert pieceCount > 0;
 		int index = -1;
 		for (int i = 0; i < pieceCount; i++) {
-			if (locs[i] == loc) {
+			if (locs[i] == location.ordinal()) {
 				index = i;
 				break;
 			}
 		}
-		if (index < pieceCount - 1) {
-			for (int i = index + 1; i < pieceCount; i++) {
-				locs[i - 1] = locs[i];
-			}
+		assert index > -1;
+		for (int i = index + 1; i < pieceCount; i++) {
+			locs[i - 1] = locs[i];
 		}
 		pieceCount--;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(locs);
+		result = prime * result + pieceCount;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final LocationTracker other = (LocationTracker) obj;
+		if (!Arrays.equals(locs, other.locs))
+			return false;
+		if (pieceCount != other.pieceCount)
+			return false;
+		return true;
 	}
 }

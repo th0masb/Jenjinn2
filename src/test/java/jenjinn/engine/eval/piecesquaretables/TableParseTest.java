72fd63c644281470ae0508f21327f2c70a74500e
Tom <t.oliver.ball@protonmail.ch>
2018-06-14 15:36:50 +0100

    Added rest of tables + parsing test

---
+++ src/test/java/jenjinn/engine/eval/piecesquaretables/TableParseTest.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package jenjinn.engine.eval.piecesquaretables;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import jenjinn.engine.enums.ChessPiece;
+import xawd.jflow.iterators.factories.IterRange;
+
+/**
+ * @author ThomasB
+ */
+class TableParseTest
+{
+	@Test
+	void test()
+	{
+		final ChessPiece p = ChessPiece.WHITE_BISHOP;
+		final PieceSquareTable expected = new PieceSquareTable(p, IterRange.to(64).map(i -> i*(1 - 2*(i % 2))).toArray());
+		assertEquals(expected, TableParser.parseFile(p, "testTable"));
+	}
+}

17596c57aef2a202400daef57514071ff82dd64b
Tom <t.oliver.ball@protonmail.ch>
2018-06-22 17:59:23 +0100

    Added python script to unzip all .zip archives in given directory

---
+++ scripts/unzip.py
@@ -0,0 +1,19 @@
+import os, sys, zipfile
+
+args = sys.argv[1:]
+if len(args) == 0:
+    print("No folder path was passed.")
+else:
+    folder_path = os.path.join(args[0])
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        print("Unzipping .zip archives in " + folder_path)
+        zips = [x for x in os.listdir(folder_path) if x.endswith('.zip')]
+        for zip_file in zips:
+            pth = os.path.join(folder_path, zip_file)
+            print('Unzipping ' + pth + '...')
+            z = zipfile.ZipFile(pth)
+            z.extractall(folder_path)
+            os.remove(pth)
+        print('Done')
+    else:
+        print('Specified path does not lead to a folder')
